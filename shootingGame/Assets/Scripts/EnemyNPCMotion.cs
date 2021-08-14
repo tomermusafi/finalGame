1cc027a31d667b1cff823e486af2ce969815d0d3
Eliko <yyyeliko@gmail.com>
2021-08-14 16:33:18 +0300

    now 2 enemy npcs have walking logic

--- shootingGame/Assets/Scripts/EnemyNPCMotion.cs
+++ shootingGame/Assets/Scripts/EnemyNPCMotion.cs
@@ -8,9 +8,9 @@ public class EnemyNPCMotion : MonoBehaviour
     private NavMeshAgent agent;
     //public GameObject startPoint;
     //public GameObject endPoint;
-    public GameObject mainPlayer;
-    bool followMainPlayer = true;
-    int changePosition = 200;
+    public GameObject followedPlayer;
+    public bool followMainPlayer = true;
+    int changePosition = 30;
     int currentPositionCount = 0;
 
     // Start is called before the first frame update
@@ -22,29 +22,33 @@ public class EnemyNPCMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(mainPlayer.GetComponent<PlayerAttributes>().health);
+        Debug.Log(followedPlayer.GetComponent<PlayerAttributes>().health);
 
         
-        if (followMainPlayer && currentPositionCount % changePosition == 0)
+        if (followMainPlayer)
         {
-            System.Random rnd = new System.Random();
-            int x = rnd.Next(1, 15);
-            int y = rnd.Next(1, 15);
-            int z = rnd.Next(1, 15);
-            agent.SetDestination(new Vector3(mainPlayer.transform.position.x, mainPlayer.transform.position.y, mainPlayer.transform.position.z));
+            if (currentPositionCount % changePosition == 0)
+            {
+                System.Random rnd = new System.Random();
+                int x = rnd.Next(10, 30);
+                int z = rnd.Next(10, 30);
+                agent.SetDestination(new Vector3(followedPlayer.transform.position.x + x,
+                    agent.transform.position.y,
+                    followedPlayer.transform.position.z + z));
+            }
 
         } else
         {
-            // follow secondary player
-            // TODO - Add secondary player and logic
-            /*agent.SetDestination(endPoint.transform.position);
-            if (agent.transform.position.x == endPoint.transform.position.x &&
-                agent.transform.position.z == endPoint.transform.position.z)
+            if (currentPositionCount % changePosition == 0)
             {
-
+                System.Random rnd = new System.Random();
+                int x = rnd.Next(0, 10);
+                int z = rnd.Next(0, 10);
+                agent.SetDestination(new Vector3(followedPlayer.transform.position.x + x,
+                    agent.transform.position.y,
+                    followedPlayer.transform.position.z + z));
             }
-            */
         }
-
+        currentPositionCount++;
     }
 }
