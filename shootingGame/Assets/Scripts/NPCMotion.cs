1f0471895e13ae7aed5b80b1d9352a9c436bd0b3
Eliko <yyyeliko@gmail.com>
2021-08-14 16:47:11 +0300

    teammate npc added with walking logic

--- shootingGame/Assets/Scripts/NPCMotion.cs
+++ shootingGame/Assets/Scripts/NPCMotion.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NPCMotion : MonoBehaviour
+public class EnemyNPCMotion : MonoBehaviour
 {
     private NavMeshAgent agent;
     //public GameObject startPoint;
