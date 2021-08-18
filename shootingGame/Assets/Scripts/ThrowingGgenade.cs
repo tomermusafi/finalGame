2497f08e017dda47bd400b1bc80ebc7821d7fd11
Tomer Musafi <tomermusafi@gmail.com>
2021-08-18 23:27:58 +0300

    add grenade throwing

---
+++ shootingGame/Assets/Scripts/ThrowingGgenade.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingGgenade : MonoBehaviour
+{
+    private bool isThrowen = false;
+    public GameObject player;
+    public GameObject grenadePrefab;
+    private AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = player.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("q")) 
+        {
+            if (!isThrowen)
+            {
+                isThrowen = true;
+                ThrowGgenade();
+            }
+        }
+    }
+    void ThrowGgenade() 
+    {
+        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Vector3 direction = player.transform.forward * 8;
+        direction.y = 3;
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.AddForce(direction, ForceMode.Impulse);
+        StartCoroutine(Explode(grenade));
+    }
+    IEnumerator Explode(GameObject grenade)
+    {
+        yield return new WaitForSeconds(2.5f);
+        grenade.transform.GetChild(1).gameObject.SetActive(true);
+        sound.Play();
+        isThrowen = false;
+        Collider[] objectsCollider = Physics.OverlapSphere(transform.position, 20);
+        for (int i = 0; i < objectsCollider.Length; i++) 
+        {
+            if (objectsCollider[i] != null)
+            {
+                Rigidbody rbo = objectsCollider[i].GetComponent<Rigidbody>();
+                if(rbo != null)
+                    rbo.AddExplosionForce(2500.0f, transform.position, 20);
+            }
+        }
+        yield return new WaitForSeconds(0.5f);
+        Destroy(grenade);
+    }
+}
