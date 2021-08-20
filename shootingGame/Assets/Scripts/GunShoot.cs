d61c09c28a011c55655e2a6f9a10e534ab43265c
Eliko <yyyeliko@gmail.com>
2021-08-20 15:15:13 +0300

    continuing

--- shootingGame/Assets/Scripts/GunShoot.cs
+++ shootingGame/Assets/Scripts/GunShoot.cs
@@ -6,10 +6,9 @@ using UnityEngine.AI;
 public class GunShoot : MonoBehaviour
 {
     private LineRenderer lr;
-    public ParticleSystem MuzzleFlash;
     public GameObject muzzle;
 
-    public GameObject aCamera;
+    public GameObject currentPlayer;
     private AudioSource sound;
     public GameObject aGun;
     public GameObject target;
@@ -19,6 +18,10 @@ public class GunShoot : MonoBehaviour
     private Animator anim1;
     private Animator anim2;
 
+    public bool isNPC;
+    int countFramesCount = 0;
+    int whenToShootFrames = 100;
+    int minusHealth = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +29,9 @@ public class GunShoot : MonoBehaviour
         sound = aGun.GetComponent<AudioSource>();
         anim1 = Enemy1.GetComponent<Animator>();
         anim2 = Enemy2.GetComponent<Animator>();
+
+        System.Random rnd = new System.Random();
+        whenToShootFrames = rnd.Next(30, 100);
     }
 
     IEnumerator Shoot()
@@ -34,8 +40,7 @@ public class GunShoot : MonoBehaviour
         lr.SetPosition(1, target.transform.position);
         sound.Play();
         lr.enabled = true;
-        //MuzzleFlash.Play();
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.01f);
         lr.enabled = false;
     }
 
@@ -43,39 +48,98 @@ public class GunShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("shoot"))
+        // Logic to NPC
+        countFramesCount++;
+        try
         {
-            sound.Play();
-            
-            RaycastHit hit;
-            //Debug.Log(Physics.Raycast(aCamera.transform.position, aCamera.transform.forward, out hit));
-            if (Physics.Raycast(aCamera.transform.position, aCamera.transform.forward, out hit))
+            if (currentPlayer.GetComponent<PlayerAttributes>().hasGun &&
+                isNPC &&
+                countFramesCount % whenToShootFrames == 0)
             {
-                target.transform.position = hit.point;
-                StartCoroutine(Shoot());
-                if (Enemy1.transform.gameObject.name == hit.transform.gameObject.name)
+                RaycastHit hit;
+                if (Physics.Raycast(currentPlayer.transform.position, currentPlayer.transform.forward, out hit))
                 {
-                    Enemy1.GetComponent<PlayerAttributes>().health -= 50;
-                    if (Enemy1.GetComponent<PlayerAttributes>().health <= 0)
-                    {
-                        Enemy1.GetComponent<NavMeshAgent>().enabled = false;
-                        Enemy1.GetComponent<PlayerAttributes>().isAlive = false;
-                    }
-                    Debug.Log("Hit enemy 1");
-                }
+                    target.transform.position = hit.point;
+                    StartCoroutine(Shoot());
 
-                if (Enemy2.transform.gameObject.name == hit.transform.gameObject.name)
-                {
-                    Enemy2.GetComponent<PlayerAttributes>().health -= 50;
-                    if (Enemy2.GetComponent<PlayerAttributes>().health <= 0)
+                    GameObject pickedEnemy = null;
+                    float pickeEnemyDistance = 9999999;
+                    if (Enemy1.GetComponent<PlayerAttributes>().isAlive)
                     {
-                        Enemy2.GetComponent<NavMeshAgent>().enabled = false;
-                        Enemy2.GetComponent<PlayerAttributes>().isAlive = false;
+                        if (Vector3.Distance(Enemy1.transform.position, currentPlayer.transform.position) < pickeEnemyDistance)
+                        {
+                            pickeEnemyDistance = Vector3.Distance(Enemy1.transform.position, currentPlayer.transform.position);
+                            pickedEnemy = Enemy1;
+                        }
                     }
-                    Debug.Log("Hit enemy 2");
-                }
 
+                    if (Enemy2.GetComponent<PlayerAttributes>().isAlive)
+                    {
+                        if (Vector3.Distance(Enemy2.transform.position, currentPlayer.transform.position) < pickeEnemyDistance)
+                        {
+                            pickeEnemyDistance = Vector3.Distance(Enemy1.transform.position, currentPlayer.transform.position);
+                            pickedEnemy = Enemy2;
+                        }
+                    }
+
+                    if (!pickedEnemy.Equals(null))
+                    {
+                        pickedEnemy.GetComponent<PlayerAttributes>().health -= minusHealth;
+                        if (pickedEnemy.GetComponent<PlayerAttributes>().health <= 0)
+                        {
+                            if (!pickedEnemy.GetComponent<PlayerAttributes>().isMainPlayer)
+                            {
+                                pickedEnemy.GetComponent<NavMeshAgent>().enabled = false;
+                                pickedEnemy.GetComponent<PlayerAttributes>().isAlive = false;
+                            }
+                        }
+                        Debug.Log($"Hit enemy {pickedEnemy.transform.gameObject.name}");
+                    }
+                }
             }
+        
+
+
+            // Logic to main player
+            if (currentPlayer.GetComponent<PlayerAttributes>().hasGun &&
+                Input.GetButtonDown("shoot"))
+            {
+                sound.Play();
+            
+                RaycastHit hit;
+                //Debug.Log(Physics.Raycast(currentPlayer.transform.position, currentPlayer.transform.forward, out hit));
+                if (Physics.Raycast(currentPlayer.transform.position, currentPlayer.transform.forward, out hit))
+                {
+                    target.transform.position = hit.point;
+                    StartCoroutine(Shoot());
+                    if (Enemy1.transform.gameObject.name == hit.transform.gameObject.name)
+                    {
+                        Enemy1.GetComponent<PlayerAttributes>().health -= minusHealth;
+                        if (Enemy1.GetComponent<PlayerAttributes>().health <= 0)
+                        {
+                            Enemy1.GetComponent<NavMeshAgent>().enabled = false;
+                            Enemy1.GetComponent<PlayerAttributes>().isAlive = false;
+                        }
+                        Debug.Log("Hit enemy 1");
+                    }
+
+                    if (Enemy2.transform.gameObject.name == hit.transform.gameObject.name)
+                    {
+                        Enemy2.GetComponent<PlayerAttributes>().health -= minusHealth;
+                        if (Enemy2.GetComponent<PlayerAttributes>().health <= 0)
+                        {
+                            Enemy2.GetComponent<NavMeshAgent>().enabled = false;
+                            Enemy2.GetComponent<PlayerAttributes>().isAlive = false;
+                        }
+                        Debug.Log("Hit enemy 2");
+                    }
+
+                }
+            }
+        }
+        catch
+        {
+
         }
     }
 }
--- shootingGame/Assets/Scripts/NPCGunShoot.cs
+++ shootingGame/Assets/Scripts/NPCGunShoot.cs
@@ -2,14 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttributes : MonoBehaviour
+public class NPCGunShoot : MonoBehaviour
 {
-    public int health;
-    public bool isAlive;
-    public bool hasGun;
-    public bool hasGrenade;
-    public bool isMainPlayer;
-
     // Start is called before the first frame update
     void Start()
     {
